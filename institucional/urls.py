458aa27bd54487025ffded9a489fcc03c37ddde6
Felipe Araujo <felipearaujo.m.g94@gmail.com>
2026-07-30 21:17:41 -0300

    Cria template base com herança e página Sobre funcionando

--- institucional/urls.py
+++ institucional/urls.py
@@ -4,3 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('sobre/', views.sobre, name='sobre'),
+]
--- institucional/views.py
+++ institucional/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'institucional/home.html')
+
+def sobre(request):
+    return render(request, 'institucional/sobre.html')
